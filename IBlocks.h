88f6677e634bead1533631ce9bee3994c8bab1e5
JT <60622543+HarryFishnuts@users.noreply.github.com>
2021-09-12 16:06:42 -0400

    Minor Updates

--- IBlocks.h
+++ IBlocks.h
@@ -59,9 +59,11 @@ enum BL_BLOCK_LOOK
 	if block is solid)
 	BLColor color1        -> rightside color of gradient
 	BLTextureHandle tHndl -> texture of BLBlock (if applicable)
-	int hasText           -> 1 for has text, 0 for no text
-	int textScale         -> scale of text
+	BLByte hasText        -> 1 for has text, 0 for no text
+	BLUInt textScale      -> scale of text
 	BLByte bText[]        -> text buffer for text
+	BLByte hasBorder      -> 1 for border, 0 for no border
+	BLUInt borderSize     -> size of border (in pixels)
 * NOTE: N/A
 *************************************************************/
 typedef struct BLBlock
@@ -71,9 +73,11 @@ typedef struct BLBlock
 	BLColor            color0;
 	BLColor            color1;
 	BLTextureHandle    tHndl;
-	int                hasText;
-	int                textScale;
+	BLByte             hasText;
+	BLUInt             textScale;
 	BLByte             bText[BL_BLOCK_TEXT_MAX];
+	BLByte             hasBorder;
+	BLUInt             borderSize;
 } BLBlock;
 
 /*************************************************************
@@ -81,9 +85,9 @@ typedef struct BLBlock
 * DATE: 2021 - 09 - 12
 * MEMBERS:
 *	enum BL_GFONT_TYPE fType -> type of text to render
-*	int    textScale    -> scale of text
-*	int    hasTextClamp -> 1 for use textClamp, 0 for no clamp
-*	int    textClamp    -> max amount of text
+*	BLUInt textScale    -> scale of text
+*	BLByte hasTextClamp -> 1 for use textClamp, 0 for no clamp
+*	BLUInt textClamp    -> max amount of text
 *	BLByte bText[]      -> text to render
 *	BLRecti dims -> dimensions
 * NOTE: N/A
@@ -91,9 +95,9 @@ typedef struct BLBlock
 typedef struct BLTextField
 {
 	enum BL_GFONT_TYPE fType;
-	int    textScale;
-	int    hasTextClamp;
-	int    textClamp;
+	BLUInt textScale;
+	BLByte hasTextClamp;
+	BLUInt textClamp;
 	BLByte bText[BL_TEXFIELD_TEXT_MAX];
 	BLRecti dims;
 } BLTextField;
@@ -102,30 +106,55 @@ typedef struct BLTextField
 * NAME: BLButton
 * DATE: 2021 - 09 - 12
 * MEMBERS:
-*	enum BL_GFONT_TYPE fType -> type of text to render
-*	int    textScale    -> scale of text
-*	int    hasTextClamp -> 1 for use textClamp, 0 for no clamp
-*	int    textClamp    -> max amount of text
-*	BLByte bText[]      -> text to render
-*	BLRecti dims -> dimensions
+* 	BLByte  hasBorder  -> 1 for border, 0 for none
+	BLUInt  borderSize -> border scale in pixels
+	BLColor fg -> button color
+	BLColor bg -> border color
+	BLByte  hasText      -> 1 for text, 0 for no text
+	BLByte  hasTextClamp -> 1 for texClamp, 0 for none
+	BLUInt  textClamp    -> maximum text size
+	BLUInt  textScale    -> text scale (in pixels)
+	BLByte  bText[]      -> text buffer to render
+	float   hoverScale   -> scale of button when overlapping
+	with mouse
+	float   clickScale   -> scale of button when clicked
+	BLRecti dims -> dimensions
 * NOTE: N/A
 *************************************************************/
 typedef struct BLButton
 {
-	int hasBorder;
-	int borderSize;
+	BLByte hasBorder;
+	BLUInt borderSize;
 	BLColor fg;
 	BLColor bg;
-	int hasText;
-	int hasTextClamp;
-	int textClamp;
-	int textScale;
+	BLByte hasText;
+	BLByte hasTextClamp;
+	BLUInt textClamp;
+	BLUInt textScale;
 	BLByte bText[BL_BUTTON_TEXT_MAX];
 	float hoverScale;
 	float clickScale;
+	BLRecti dims;
 };
 
-
+/*************************************************************
+* NAME: BLCreateRecti
+* DATE: 2021 - 09 - 12
+* PARAMS:
+*	enum BL_BLOCK_LOOK bLook -> how the BLBlock will be rendered
+*	BLRecti bDims  -> dims of the BLBlock
+*	BLColor* bCols -> 2 BLColors, first being the main color,
+* second being the border color. If the block is a gradient, the
+* first is the left color, and second is the right color
+*	BLUInt bSize  -> border size (in pixels), NULL for no border
+*	BLUInt tScale -> text scale	
+*	BLByte* tBuf  -> text buffer (must be null terminated)
+* 
+* RETURNS:
+*	BLBlock create from params
+* NOTE: N/A
+*************************************************************/
+BLBlock BLICreateBlock(enum BL_BLOCK_LOOK bLook, BLRecti bDims, BLColor* bCols, BLUInt bSize, BLUInt tScale, BLByte* tBuf);
 
 
 #endif
