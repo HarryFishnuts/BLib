898e3893bb21356e0fd71e674b0f65c8dec1378c
JT <60622543+HarryFishnuts@users.noreply.github.com>
2021-09-12 16:22:39 -0400

    BLib header updated

--- BLib.h
+++ BLib.h
@@ -15,7 +15,24 @@
 
 #include "GCore.h"   // Basic graphics
 #include "GTexLib.h" // Texture editing
+#include "GFont.h"   // Basic fonts
+#include "ICore.h"   // Basic user interface
 
 #include "WCore.h"   // Basic window handling
+
+/*************************************************************
+* NAME: BLibInit
+* DATE: 2021 - 09 - 12
+* PARAMS:
+*	BLUInt width  -> width of main window
+*	BLUInt height -> height of main window
+*
+* RETURNS:
+*	WindowHandle of main window
+* NOTE:
+*	Inits all BLib modules and creates a main window context
+	for all font and rendering.
+*************************************************************/
+BLWindowHandle BLibInit(BLUInt width, BLUInt height);
 
 #endif 
